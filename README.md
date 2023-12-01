5ac1763377a07abf30988c512c4b67f62add9ba6
SmallPig <1424928981@qq.com>
2023-12-01 21:31:04 +0800

    add readme.md add docker-compose.yml add Dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 
+##  项目结构
 
+```shell
+├─.idea
+├─api
+├─conf
+├─database
+│  ├─cache
+│  └─db
+│      ├─dao
+│      └─model
+├─es
+├─middleware
+├─mq
+├─response
+├─route
+├─service
+├─types
+└─util
+```
+- api 放置路由相关接口
+- conf mysql， redis，oss等相关配置
+- database mysql，redis相关操作
+- es elastic操作
+- middleware JWT认证操作
+- mq 聊天模块的操作，使用rabbitmq实现
+- response 返回数据结构
+- route 路由组
+- service 具体的各种操作
+- types 存放请求相关结构体
+- util 生成token
+
+
+###  项目完成情况
+
+- bonus部分完成了1，2，6，7
+
+
+### 接口文档地址
+
+- https://apifox.com/apidoc/shared-2f99121d-895b-4576-a9f2-2e985a8087e9
--- conf/conf.go
+++ conf/conf.go
@@ -4,27 +4,27 @@ const (
 	MySqlUser     = "root"
 	MySqlPassword = "123456"
 	MySqlDataBase = "videoweb"
-	MysqlIP       = "192.168.242.139:3306"
+	MysqlIP       = "192.168.136.128:3306"
 )
 
 const (
 	RabbitmqUser     = "admin"
 	RabbitmqPassword = "123456"
-	RabbitmqIP       = "192.168.242.139:5672"
+	RabbitmqIP       = "192.168.136.128:5672"
 	ExchangeName     = "direct"
 )
 const (
-	RedisAddr     = "192.168.242.139:6379"
+	RedisAddr     = "192.168.136.128:6379"
 	RedisPassword = "123456"
 	RedisDB       = 2 //redis的连接池
 )
 
 const (
-	EsUrl = "http://192.168.242.139:9200"
+	EsUrl = "http://192.168.136.128:9200"
 )
 const (
-	OSS_END_POINT         = "xxx"
-	OSS_ACCESS_KEY_ID     = "xxx"
-	OSS_ACCESS_KEY_SECRET = "xxx"
-	OSS_BUCKET            = "xxx"
+	OSS_END_POINT         = "oss-cn-hongkong.aliyuncs.com"
+	OSS_ACCESS_KEY_ID     = "LTAI5tB7Jjsxa6GdseViWqRE"
+	OSS_ACCESS_KEY_SECRET = "ABrFBE62QML2oiBeJe0j7QBQm0ADrJ"
+	OSS_BUCKET            = "videoweb-test"
 )
